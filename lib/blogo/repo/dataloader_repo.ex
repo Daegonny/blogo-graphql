3d1faf855fcf2987cb1c56d84d432badddb2b778
Daegonny <igorchagasm@hotmail.com>
2022-11-06 16:41:43 -0300

    refact: extract general purpouse function

--- lib/blogo/repo/dataloader_repo.ex
+++ lib/blogo/repo/dataloader_repo.ex
@@ -1,10 +1,19 @@
 defmodule Blogo.Repo.DataloaderRepo do
   @moduledoc false
+  import Absinthe.Resolution.Helpers, only: [on_load: 2]
   alias Blogo.Repo
 
   def query(queryable, _), do: queryable
 
-  def data() do
-    Dataloader.Ecto.new(Repo, query: &query/2)
+  @spec data :: Dataloader.Ecto.t()
+  def data(), do: Dataloader.Ecto.new(Repo, query: &query/2)
+
+  def by_parent(parent, field, args, loader) do
+    loader
+    |> Dataloader.load(__MODULE__, {field, args}, parent)
+    |> on_load(fn loader ->
+      children = Dataloader.get(loader, __MODULE__, {field, args}, parent)
+      {:ok, children}
+    end)
   end
 end
