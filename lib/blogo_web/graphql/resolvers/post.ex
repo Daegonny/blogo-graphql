ec449d16860f53fedc217f0856801b24096ed69e
Daegonny <igorchagasm@hotmail.com>
2022-11-06 16:16:06 -0300

    feat: allow query posts from author

--- lib/blogo_web/graphql/resolvers/post.ex
+++ lib/blogo_web/graphql/resolvers/post.ex
@@ -2,7 +2,9 @@ defmodule BlogoWeb.Graphql.Resolvers.Post do
   @moduledoc """
   Post graphql resolver
   """
-  alias Blogo.Posts
+  import Absinthe.Resolution.Helpers, only: [on_load: 2]
+  alias Blogo.{Author, Posts}
+  alias Blogo.Repo.DataloaderRepo
 
   def get(_root, %{id: id}, _info) do
     case Posts.get(id) do
@@ -13,5 +15,14 @@ defmodule BlogoWeb.Graphql.Resolvers.Post do
 
   def all(_root, _params, _info) do
     {:ok, Posts.all()}
+  end
+
+  def by_author(%Author{} = author, args, %{context: %{loader: loader}}) do
+    loader
+    |> Dataloader.load(DataloaderRepo, {:posts, args}, author)
+    |> on_load(fn loader ->
+      posts = Dataloader.get(loader, DataloaderRepo, {:posts, args}, author)
+      {:ok, posts}
+    end)
   end
 end
--- lib/blogo_web/graphql/types/author.ex
+++ lib/blogo_web/graphql/types/author.ex
@@ -3,11 +3,16 @@ defmodule BlogoWeb.Graphql.Types.Author do
   Author graphql object
   """
   use Absinthe.Schema.Notation
+  alias BlogoWeb.Graphql.Resolvers
 
   object :author do
     field(:id, non_null(:id))
     field(:name, non_null(:string))
     field(:age, non_null(:integer))
     field(:country, non_null(:string))
+
+    field(:posts, non_null(list_of(:post))) do
+      resolve(&Resolvers.Post.by_author/3)
+    end
   end
 end
